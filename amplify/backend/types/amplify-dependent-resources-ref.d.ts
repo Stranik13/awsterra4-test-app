e7f826eeee87fc864de804d9064954793aacf4c8
OlehKondratov <awsterra4@gmail.com>
2022-09-01 22:50:36 +0200

    start

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,12 @@
+export type AmplifyDependentResourcesAttributes = {
+    "storage": {
+        "testappdb": {
+            "Name": "string",
+            "Arn": "string",
+            "StreamArn": "string",
+            "PartitionKeyName": "string",
+            "PartitionKeyType": "string",
+            "Region": "string"
+        }
+    }
+}
